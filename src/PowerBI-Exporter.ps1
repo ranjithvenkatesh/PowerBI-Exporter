476b05a1c5f75fc9c1b09368b9b20ebabfa61b2a
Ranjith Venkatesh <ranjith.venkatesh@mossandlichens.com>
2022-01-22 17:32:18 +0100

    Download PBIX file

--- src/PowerBI-Exporter.ps1
+++ src/PowerBI-Exporter.ps1
@@ -3,8 +3,10 @@
 #Backup of all Power BI Reports in Workspace
 #Backup of all Power BI Report in multiple Workspaces
 param(
+    [string]$ProjectFolder,
     [string]$Environment,
-    [string]$ProjectFolder
+    [string]$ReportID,
+    [string]$ReportName
     )
 
 # Power BI REST API URIS
@@ -25,4 +27,9 @@ $Payload = @{
 Write-HostLog -Message "Getting access token from Power BI Service..."
 $PowerBI_TOKEN_RESPONSE = Invoke-RestMethod -Method Post -Uri $PowerBI_REST_API_TOKEN -Body $Payload
 $PowerBI_TOKEN_JSON = $PowerBI_TOKEN_RESPONSE.token_type + ' ' + $PowerBI_TOKEN_RESPONSE.access_token
-Write-HostLog -Message "Access token:$PowerBI_TOKEN_JSON"
+ 
+Write-HostLog -Message "Exporting Report..."
+$PowerBI_REPORT_EXPORT_API = "https://api.powerbi.com/v1.0/myorg/reports/" + $ReportID + "/Export"
+$PowerBI_REPORT_Name = $ReportName + ".pbix"
+Invoke-RestMethod -Method Get -Uri $PowerBI_REPORT_EXPORT_API -ContentType 'application/zip' -Headers @{authorization = $PowerBI_TOKEN_JSON} -OutFile $PowerBI_REPORT_Name
+Write-HostLog -Message "Report exported."
